ae025df80aadefbdc16da2aebaf0a88cc28a0e1f
jeromeOlivier <jerome.olivier@icloud.com>
2022-11-23 09:17:56 -0500

    tried different grid layouts and played around with svg files

---
+++ fe-gradient/script.js
@@ -0,0 +1,12 @@
+// const sections = document.querySelectorAll("section");
+// const observer = new IntersectionObserver(entries => {
+//   entries.forEach(entry => {
+//     entry.target.classList.toggle("show", entry.isIntersecting)
+//   })
+// }, {
+//   threshold: .5,
+// });
+//
+// sections.forEach(section => {
+//   observer.observe(section)
+// });
