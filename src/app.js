f8ff96985dbd647b87e490c6b33cf14549d66ae4
jeromeOlivier <jerome.olivier@icloud.com>
2023-10-12 06:03:06 -0400

    testing 0.0.0.0 once more

--- src/app.js
+++ src/app.js
@@ -47,4 +47,4 @@ app.use("/", routes);
 
 // Initialize server
 const port = process.env.PORT || "3000";
-app.listen(port, () => console.log(`Server running on port ${ port }`));
+app.listen(port, "0.0.0.0", () => console.log(`Server running on port ${ port }`));
